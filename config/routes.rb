792ea2107c317d5e934b8ad5d659d94f79c720ba
github username <github email address>
2018-03-05 14:09:57 -0500

    forgot to expose seeds route

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
       post '/bookshelf_list', to: "bookshelves#list"
 
       get '/test', to: 'users#test'
-      # get '/seed', to: "request#fetch_data"
+      get '/seed', to: "request#fetch_data"
     end
   end
 end
